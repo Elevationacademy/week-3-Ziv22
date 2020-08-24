ba05eaee5403d903702457cc1bf2def17a8c3926
ziv221 <ziv.itzh@gmail.com>
2020-08-24 16:43:45 +0300

    finished api excercises

--- day_2/Ajajx/Google_Books_API_Extension/main.js
+++ day_2/Ajajx/Google_Books_API_Extension/main.js
@@ -1,2 +1,34 @@
-const src = $("#books-list-template").html()
-const template = gandle
+let data;
+const neruoscienceComputerBooks = [];
+
+const pushBooks = book =>{
+    if(book.volumeInfo.categories){
+        if(book.volumeInfo.categories.includes('Computers')){
+            neruoscienceComputerBooks.push(book.volumeInfo.title)
+        }
+    }
+}
+
+const fetchBooks = (startIndex) =>{
+    $.ajax({
+        method: "GET",
+        url: `https://www.googleapis.com/books/v1/volumes?q=title:neuroscience&maxResults=40&startIndex=${startIndex}`,
+        success: function (data) {
+            data.items.forEach(element => {
+                pushBooks(element) 
+            });
+        },  
+        error: function (xhr, text, error) {
+            console.log(text);
+        }
+    }); 
+}
+
+const generateNeruoscienceArray = (runsNum) =>{
+    for (let i = 0; i < runsNum; i+=40) {
+        fetchBooks(i);
+    }
+}
+
+
+generateNeruoscienceArray(121);
